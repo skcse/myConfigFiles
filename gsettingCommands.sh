11be008b2ba5df75460ee2a19394b5fc1c702bfd
shashwat-r <shashwat-r@users.noreply.github.com>
2017-12-11 18:01:57 +0530

    created file

---
+++ gsettingCommands.sh
@@ -0,0 +1,8 @@
+# enable natural scrolling
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
+# enable minimize on click [true|false]
+gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
+# enable focus on hover ['click'|'sloppy'|'mouse']
+gsettings set org.gnome.desktop.wm.preferences focus-mode 'mouse'
+# move taskbar to left [Left|Bottom]
+gsettings set com.canonical.Unity.Launcher launcher-position Left
